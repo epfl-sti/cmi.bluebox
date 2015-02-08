5c1c85cad4494cfb8271e8356661cd1c09de333b
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2015-02-08 13:11:21 +0100

    Support multiple commands, e.g. run_noc.sh restart shell

--- run_noc.sh
+++ run_noc.sh
@@ -30,20 +30,23 @@ is_running() {
     test 0 != $(docker ps -q "$BLUEBOXNOC_DOCKER_NAME" | wc -l)
 }
 
-case "$1" in
-    status)
-        docker ps "$BLUEBOXNOC_DOCKER_NAME" ;;
-    start)
-        start ;;
-    stop)
-        stop ;;
-    shell)
-        is_running || {
-          echo >&2 "$BLUEBOXNOC_DOCKER_NAME" is not running
-          exit 2
+while [ -n "$1" ]; do
+    case "$1" in
+        status)
+            docker ps "$BLUEBOXNOC_DOCKER_NAME" ;;
+        start)
+            start ;;
+        stop)
+            stop ;;
+        shell)
+            is_running || {
+            echo >&2 "$BLUEBOXNOC_DOCKER_NAME" is not running
+            exit 2
         }
-        docker exec -it $(docker ps -q "$BLUEBOXNOC_DOCKER_NAME") bash ;;
-    restart)
-        stop
-        start ;;
-esac
+            docker exec -it $(docker ps -q "$BLUEBOXNOC_DOCKER_NAME") bash ;;
+        restart)
+            stop
+            start ;;
+    esac
+    shift
+done
