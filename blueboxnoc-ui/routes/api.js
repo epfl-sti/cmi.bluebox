b7c89bb4bafe83900925c8c86de2a950a37b139f
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-09 16:38:23 +0100

    Serving dashboard + details of VPNs

--- blueboxnoc-ui/routes/api.js
+++ blueboxnoc-ui/routes/api.js
@@ -5,9 +5,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* TODO: de-bogosify */
 router.get('/vpn', function(req, res, next) {
-    res.json([{id: 1, title: "Foo"}, {id: 1, title: "Bar"}, {id: 2, title: "Baz"}]);
+    res.json([{id: 1, title: "Foo"}, {id: 2, title: "Bar"}, {id: 3, title: "Baz"}]);
+});
+
+router.get('/vpn/1', function(req, res, next) {
+    res.json({id: 1, title: "Foo"});
+});
+
+router.get('/vpn/2', function(req, res, next) {
+    res.json({id: 2, title: "Bar"});
+});
+
+router.get('/vpn/3', function(req, res, next) {
+    res.json({id: 3, title: "Baz"});
 });
 
 module.exports = router;
