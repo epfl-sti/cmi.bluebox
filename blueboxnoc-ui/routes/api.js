2a510fb4f5fe49f452969cf4646abf77ce278408
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-09 16:50:44 +0100

    Figured out how to parse an URL.

--- blueboxnoc-ui/routes/api.js
+++ blueboxnoc-ui/routes/api.js
@@ -10,16 +10,10 @@ router.get('/vpn', function(req, res, next) {
     res.json([{title: "Foo", detail: "Foofoo"}, {title: "Bar", detail: "Foobar"}, {title: "Baz", detail: "Foobaz"}]);
 });
 
-router.get('/vpn/Foo', function(req, res, next) {
-    res.json({title: "Foo", detail: "Foofoo"});
-});
-
-router.get('/vpn/Bar', function(req, res, next) {
-    res.json({title: "Bar", detail: "Foobar"});
-});
-
-router.get('/vpn/Baz', function(req, res, next) {
-    res.json({title: "Baz", detail: "Foobaz"});
+router.get('/vpn/*', function(req, res, next) {
+    var urlparts = req.url.split("/");
+    var stem = urlparts.pop();
+    res.json({title: stem, detail: "Foo" + stem});
 });
 
 module.exports = router;
