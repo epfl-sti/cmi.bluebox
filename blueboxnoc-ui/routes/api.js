27ead92e6bdc10021213b59cd56c67f8745a65ea
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-09 16:18:41 +0100

    Working list of VPNs (with mock data)

--- blueboxnoc-ui/routes/api.js
+++ blueboxnoc-ui/routes/api.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/vpn', function(req, res, next) {
-    res.json([{id: "Foo"}]);
+    res.json([{id: 1, title: "Foo"}, {id: 1, title: "Bar"}, {id: 2, title: "Baz"}]);
 });
 
 module.exports = router;
