970676a2a89fd3d977c48d914adf92bb0d324a8f
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-09 15:27:34 +0100

    Hello world in AngularJS + bower (and imported ng-admin for good measure)

--- blueboxnoc-ui/app.js
+++ blueboxnoc-ui/app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.locals.pretty = true;  // No munging whitespace
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// https://stackoverflow.com/questions/21821773/configure-node-express-to-serve-static-bower-components
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
 app.use('/users', users);
