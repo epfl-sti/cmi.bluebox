6eaa46f74234df27713454114bc9f1267e6206ce
ponsfrilus <ponsfrilus@gmail.com>
2015-02-18 12:37:10 +0100

    Refactoring

--- blueboxnoc-ui/routes/index.js
+++ blueboxnoc-ui/routes/index.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var router = express.Router();
-var VPNModel = require("../model/vpn.js");
-var BBXModel = require("../model/bbx.js");
-var VNCModel = require("../model/vnc.js");
-var USERModel = require("../model/user.js");
+var express = require('express'),
+    router = express.Router(),
+    VPNModel = require("../model/vpn.js"),
+    BBXModel = require("../model/bbx.js"),
+    VNCModel = require("../model/vnc.js"),
+    USERModel = require("../model/user.js"),
+    GROUPModel = require("../model/group.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-      title: 'BlueBoxNOC',
-      VPNTitleValidator: String(VPNModel.validName),
-      BBXTitleValidator: String(BBXModel.validName),
-      VNCTitleValidator: String(VNCModel.validName),
-      USERTitleValidator: String(USERModel.validName)});
+      title: 'BlueBoxNOC' });
 });
 router.get('/js/validations.js', function(req, res, next) {
     res.writeHead(200, {"Content-Type": "text/javascript"});
