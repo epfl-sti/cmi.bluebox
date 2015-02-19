7cb009678475b7258a42d3ae028b85f14e5bc4c6
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-19 10:45:36 +0100

    De-duplicate code in api.js

--- blueboxnoc-ui/routes/api.js
+++ blueboxnoc-ui/routes/api.js
@@ -7,65 +7,38 @@ var router = express.Router();
 
 var Model = require("../model");
 
-router.get('/vpn', function(req, res, next) {
-    res.json(Model.sort(Model.VPN.all(),
-                        req.query._sortField, req.query._sortDir));
-});
-
-router.get('/vpn/*', function(req, res, next) {
-    var urlparts = req.url.split("/");
-    var stem = urlparts.pop();
-    Model.VPN.validName(stem);
-    Model.VPN.all().forEach(function (value, index) {
-        if (value.name == stem) {
-            return res.json(value);
-        }
+/**
+ * Configure the router object to serve the API for a model class.
+ * @param router The express.Router object
+ * @param api_path The URI sub-path to the API, e.g. "/vpn"
+ * @param model One of the model.Foo classes
+ */
+function configure_API_subdir(router, api_path, model) {
+    /* Serve e.g. /vpn */
+    router.get(api_path, function(req, res, next) {
+        res.json(Model.sort(model.all(),
+                            req.query._sortField, req.query._sortDir));
     });
-});
 
-router.get('/bbx', function(req, res, next) {
-    res.json(Model.sort(Model.BBox.all(), req.query._sortField, req.query._sortDir));
-});
-
-router.get('/bbx/*', function(req, res, next) {
-    var urlparts = req.url.split("/");
-    var stem = urlparts.pop();
-    Model.BBox.validName(stem);
-    Model.BBox.all().forEach(function (value, index) {
-        if (value.name == stem) {
-            return res.json(value);
+    /* Serve e.g. /vpn/* */
+    router.get(api_path + '/*', function(req, res, next) {
+        var urlparts = req.url.split("/");
+        var stem = urlparts.pop();
+        if (model.validName) {
+            model.validName(stem);
         }
+        /* There has to be a better way than an exhaustive search here. */
+        model.all().forEach(function (value, index) {
+            if (value.name == stem) {
+                return res.json(value);
+            }
+        });
     });
-});
+}
 
-router.get('/vnc', function(req, res, next) {
-    res.json(Model.sort(Model.VNCTarget.all(), req.query._sortField, req.query._sortDir));
-});
-
-router.get('/vnc/*', function(req, res, next) {
-    var urlparts = req.url.split("/");
-    var stem = urlparts.pop();
-    Model.VNCTarget.validName(stem);
-    Model.VNCTarget.all().forEach(function (value, index) {
-        if (value.name == stem) {
-            return res.json(value);
-        }
-    });
-});
-
-
-router.get('/user', function(req, res, next) {
-    res.json(Model.sort(Model.User.all(), req.query._sortField, req.query._sortDir));
-});
-
-router.get('/user/*', function(req, res, next) {
-    var urlparts = req.url.split("/");
-    var stem = urlparts.pop();
-    Model.User.all().forEach(function (value, index) {
-        if (value == stem) {
-            return res.json(value);
-        }
-    });
-});
+configure_API_subdir(router, "/vpn", Model.VPN);
+configure_API_subdir(router, "/vnc", Model.VNCTarget);
+configure_API_subdir(router, "/bbx", Model.BBox);
+configure_API_subdir(router, "/user", Model.User);
 
 module.exports = router;
