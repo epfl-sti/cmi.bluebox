bda5d55570e2e02d48d1a1dfe3b6264aec8e3243
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-19 16:43:54 +0100

    Start _json.js + test suite

---
+++ blueboxnoc-ui/model/_json.js
@@ -0,0 +1,40 @@
+/**
+ * Support models materialized in JSON classes
+ */
+
+var assert = require("assert"),
+    path = require("path"),
+    fs = require("fs"),
+    readdirp = require("readdirp");
+
+
+module.exports.loadAllConfigs = function(dir, done) {
+    var results = {};
+    readdirp({ root: dir, fileFilter: 'config.json' })
+        .on('warn', function (err) {
+            console.error('loadAllConfigs: warning: ', err);
+            // optionally call stream.destroy() here in order to abort and cause 'close' to be emitted
+        })
+        .on('error', function (err) {
+            console.error('loadAllConfigs: fatal: ', err);
+            done(null, err);
+            done = undefined;  // Prevents double call
+        })
+        .on('end', function() {
+            done(results);
+            done = undefined;
+        })
+        .on('data', function (entry) {
+            var insertInto = results;
+            var pathElems = entry.path.split(path.sep);
+            assert.equal(pathElems.pop(), "config.json");
+            pathElems.forEach(function (pathElem) {
+                insertInto[pathElem] = insertInto[pathElem] || {};
+                insertInto = insertInto[pathElem];
+            });
+            var data = JSON.parse(fs.readFileSync(entry.fullPath));
+            Object.keys(data).forEach(function (k) {
+                insertInto[k] = data[k];
+            })
+        });
+};
