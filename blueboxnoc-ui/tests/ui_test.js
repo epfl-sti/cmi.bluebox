bb676b5e908295b0d19d87de7b42223c642da528
Dominique Quatravaux <dominique@quatravaux.org>
2015-02-21 21:11:36 +0100

    it('shows a complete dashboard')

--- blueboxnoc-ui/tests/ui_test.js
+++ blueboxnoc-ui/tests/ui_test.js
@@ -2,8 +2,44 @@
  * Test the UI.
  */
 var assert = require('assert'),
+    debug = require('debug')('ui_test'),
     testlib = require('./testlib'),
     webdriver = require('selenium-webdriver');
+
+debug.printXPath = function (opt_text, elem) {
+    var debug = this;
+    if (! debug.enabled) return;
+
+    if (! elem) {
+        elem = opt_text;
+        opt_text = "";
+    }
+    testlib.WebdriverTest.getXPath(elem)
+        .then(function (xpath) {
+            debug(opt_text + xpath);
+        });
+};
+
+function findLinkByText(driverOrElement, text) {
+    return driverOrElement.findElement(webdriver.By.linkText(text));
+}
+
+function findText(driverOrElement, text) {
+    return driverOrElement.findElement(webdriver.By.xpath(
+        '*[contains(., "' + text + '")]'));
+}
+
+function findDashboardWidget(driverOrElement, title) {
+    driverOrElement.wait(function () {
+        return driverOrElement.isElementPresent(webdriver.By.linkText(title));
+    });
+    return findLinkByText(driverOrElement, title)
+        .then(function (elem) {
+            debug.printXPath(
+                "XPath to link containing '" + title + "': ", elem);
+            return elem.findElement(webdriver.By.xpath('ancestor::ma-dashboard-panel'));
+        });
+}
 
 testlib.WebdriverTest.describe('Read-only navigation', function() {
     this.setUpFakeData();
@@ -22,5 +58,41 @@ testlib.WebdriverTest.describe('Read-only navigation', function() {
         driver.wait(function () {
             return driver.isElementPresent(webdriver.By.linkText("BlueBoxNOC_Admins"));
         });
+    });
+
+    it('shows a complete dashboard', function () {
+        var driver = this.driver;
+        driver.get("/");
+
+        function findInDashboardWidget(dashboardTitle, opts) {
+            findDashboardWidget(driver, dashboardTitle)
+                .then(function (widgetElem) {
+                    var linkTexts = opts.linkTexts || [];
+                    linkTexts.forEach(function (linkText) {
+                        findLinkByText(widgetElem, linkText);
+                    });
+
+                    var texts = opts.texts || [];
+                    texts.forEach(function (text) {
+                        findText(widgetElem, text);
+                    })
+                });
+        }
+
+        findInDashboardWidget("VPNs List",
+            {
+                linkTexts: ["VPN", "Description", "Bax"],
+                texts: ["Foobar"]
+            });
+        findInDashboardWidget("BlueBoxes List",
+            {
+                linkTexts: ["BBX", "Description", "VPN", "Status", "bboo2"],
+                texts: ["Booboo2"]
+            });
+        findInDashboardWidget("VNCs List",
+            {
+                linkTexts: ["VNC", "Description", "VPN", "vnc2"],
+                texts: ["detail of my second vnc"]
+            });
     });
 });
